97ab25fe2039e79f266e72346e2836417cee5a47
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-08-22 12:21:26 -0300

    variaveis da maquina virtual

--- js/vm.js
+++ js/vm.js
@@ -1,7 +1,30 @@
+/* Variavel que contem todo contudo do arquivo.obj */
+let fileObj;
 
+/* Variavel de controle para percorrer a pilha de memoria */
+let s = -1;
 
+/* Variavel de controle para percorrer a lista de instruções a serem executadas */
+let i = 0;
 
+/* Vetor responsavel por simular a pilha de memoria */
+let memory = [];
 
-function openFile(){
+/* Vetor responsavel por simular a lista de instruções a serem executadas */
+let instructions = [];
 
+/* Variavel de controle para identificar qual operação deve ser executada */
+let operation = '';
+
+/* Variavel de controle para identificar quando o programa deve finalizar a execução */
+let exec = true;
+
+/* Contador para lista de instruções */
+let numInst = 1;
+
+function main(){
+    
 }
+
+
+
