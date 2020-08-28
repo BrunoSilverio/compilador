1605b6fc837e7d57fd778df83989bcf9afce3430
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-08-28 10:23:55 -0300

    front ajustado + JS operations

--- js/vm.js
+++ js/vm.js
@@ -22,8 +22,100 @@ let exec = true;
 /* Contador para lista de instruções */
 let numInst = 1;
 
+/* Funcao  */
 function main(){
-    
+    while (condition) {
+        operation = instructions[i].operation;
+
+        switch (operation) {
+            case "LDC":
+                ldc();
+                break;
+            case "LDV":
+                ldv();
+                break;
+            case "ADD":
+                add();
+                break;
+            case "SUB":
+                sub();
+                break;
+            case "MULT":
+                mult();
+                break;
+            case "DIVI":
+                divi();
+                break;
+            case "INV":
+                inv();
+                break;
+            case "AND":
+                and();
+                break;
+            case "OR":
+                or();
+                break;
+            case "NEG":
+                neg();
+                break;
+            case "CME":
+                cme();
+                break;
+            case "CMA":
+                cma();
+                break;    
+            case "CEQ":
+                ceq();
+                break;
+            case "CDIF":
+                cdif();
+                break;
+            case "CMEQ":
+                cmeq();
+                break;
+            case "CMAQ":
+                cmaq();
+                break;
+            case "START":
+                start();
+                break;
+            case "HLT":
+                hlt();
+                break;
+            case "STR":
+                str();
+                break;
+            case "JMP":
+                jmp();
+                break;
+            case "JMPF":
+                jmpf();
+                break;
+            case "NULL":
+                break;
+            case "RD":
+                rd();
+                break;
+            case "PRN":
+                prn();
+                break;
+            case "ALLOC":
+                alloc();
+                break;
+            case "DALLOC":
+                dalloc();
+                break;
+            case "CALL":
+                call();
+                break;
+            case "RETURN":
+                retn();
+                i = i-1;
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 
