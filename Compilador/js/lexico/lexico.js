3fbcd4abc3a19815ea236d7ac70a41dc38637423
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-09-14 21:47:07 -0300

    lexico/pasta excecao

---
+++ Compilador/js/lexico/lexico.js
@@ -0,0 +1,20 @@
+import * as token from './lexico/token.js';
+import * as main from '../main.js';
+
+export function lexico(programa) {
+    let posicao = 0;
+
+    //Loop para andar pelo programa, linha por linha
+    for (let index = 0; index < programa.length; index++) {
+        //Variavel auxiliar
+        let linha = programa[index];
+        
+        for (posicao = 0; posicao < linha.length; posicao++) {
+            
+            
+        }
+    }
+}
+
+
+
--- Compilador/js/main.js
+++ Compilador/js/main.js
@@ -2,3 +2,22 @@
 
 import * as lexico from './lexico/lexico.js';
 import * as token from './lexico/token.js';
+
+//Variavel com o codigo fonte completo
+let programa;
+//Variavel para guardar os tokens do codigo
+export let tokens = [];
+export let tabelaSimbolos = [];
+
+function main() {
+    try {
+        lexico.lexico(programa);
+        
+    } catch (error)
+    {
+        //Responsavel por exibir a mensagem de erro para o usuario
+
+    }
+}
+
+
