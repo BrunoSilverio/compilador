8861b1e6f730e9caea98bd5220a26eadf1612408
Daniel Fraga <dafdanielfraga@hotmail.com>
2020-09-16 23:16:22 -0300

    lexico added switch case

--- Compilador/js/lexico/lexico.js
+++ Compilador/js/lexico/lexico.js
@@ -1,20 +1,75 @@
-import * as token from './lexico/token.js';
-import * as main from '../main.js';
+//import * as token from './lexico/token.js';
+//import * as main from '../main.js';
 
-export function lexico(programa) {
+var programa = 'se contador > 10 {teste comentario}\n' +
+    'entao escreva (contador)\n' +
+    'senao escreva (x)';
+//para testar, chama a funcao do lexico e envia o programa
+lexico(programa);
+
+
+function lexico(programa) {
+
+    let comentario = false;
     let posicao = 0;
-
+    let linha = 0;
+    var atual;
+    console.log("\n" + programa);
     //Loop para andar pelo programa, linha por linha
     for (let index = 0; index < programa.length; index++) {
         //Variavel auxiliar
         let linha = programa[index];
-        
+        //console.log(linha);
+
         for (posicao = 0; posicao < linha.length; posicao++) {
-            
-            
+            //console.log(linha[posicao]);
+            atual = linha[posicao];
+            console.log(atual);
+
+            switch (atual) {
+
+                case (atual == " " || atual == "\t" || atual == "\n"):
+                    console.log("espaco ou \\t");
+                    //caso seja espaco ou \t, sai
+                    break;
+
+                case (comentario == true):
+                    if (linha[posicao] == "}") {
+                        //final de comentario 1
+                        comentario = false;
+                        console.log("fecha comentario");
+                    }
+                    if (linha[posicao] == "*" && linha[posicao + 1] == "/") {
+                        //TALVEZ AQUI: posicao atualiza pois ja validou a posicao seguinte
+                        posicao = posicao + 1;
+                        //final de comentario 2
+                        comentario = false;
+                    }
+                    break;
+
+                case (atual == "{"):
+                    //inicio de comentario 1
+                    comentario = true;
+                    console.log("abre comentario");
+                    break;
+
+                case (linha[posicao] == "/" && linha[posicao + 1] == "*"):
+                    //inicio de comentario 2
+                    comentario = true;
+                    //TALVEZ AQUI: posicao atualiza pois ja validou a posicao seguinte
+                    posicao = posicao + 1;
+                    break;
+
+
+                default: //console.log("default")
+                    break;
+            }
+
         }
     }
 }
+
+
 
 
 
