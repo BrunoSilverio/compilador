8447644f72a82ae83877e5361206e661b5a17b95
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-10-20 14:22:36 -0300

    criaçao funcao semantico

---
+++ Compilador/js/semantico/semantico.js
@@ -0,0 +1,14 @@
+// Compilador, copyright (c) by Bruno Camilo Silverio & Daniel de Arruda Fraga
+
+/*
+    • Objetivo: captar significado das ações do código fonte
+    • É uma fase de difícil formalização →  especificações informais.
+    • Utiliza a tabela de símbolos construída pelos outros analisadores.
+    • Principais funções:
+        – Manter informações sobre o escopo dos identificadores
+        – Representar tipos de dados
+*/
+
+function semantico() {
+    
+}
