141669208427d457ef4ac21d977c49d310179645
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-11-12 18:59:30 -0300

    fodase

--- maquina-virtual/js/vm.js
+++ maquina-virtual/js/vm.js
@@ -2,6 +2,7 @@
 
 // Variavel que contem todo contudo do arquivo.obj
 let arquivo;
+let comando;
 
 //Variavel de controle para percorrer a pilha de memoria, indicando elemento do topo da pilha: M[s]
 let s = -1;
@@ -25,22 +26,21 @@ let exec = true;
 // Contador para lista de instruções
 let numInst = 1;
 
-//Iniciar programa principal
-function start() {
-    s = -1;
-    //document.getElementById("formDados").innerHTML = "> ";
-    document.getElementById('compilar').addEventListener('click', function () {
-        main();
-    });
-}
-
 // Função main controla um loop para as operações das instruções
 function main() {
-    while (condition) {
-        operation = instructions[i].operation;
+    console.log("VM executada");
+    let auxNum = 0;     //auxiliar para pegar primeiro/unico numero na linha
+    let auxNum2 = 0;    //auxiliar para pegar o segundo numero na linha 
+    let auxStr = "";    //auxiliar para desvios na linha
+    let teste = arquivo.split("\n"); // define que linhas devem ser consultadas
+    
+    for (let j; j < teste.length; j++) {
+        let comando = teste[j];// espaços TAB definem colunas que serão consultadas
+        console.log(comando);
 
-        switch (operation) {
+        switch (comando) {
             case "LDC":
+
                 ldc();
                 break;
             case "LDV":
@@ -126,8 +126,7 @@ function main() {
                 break;
             default:
                 break;
-        }
-
+        } 
     }
 }
 
@@ -143,16 +142,17 @@ function readFile() {
         tabelaInstrucoes(texto);
     };
     fileReader.readAsText(fileToLoad, "UTF-8");
+
+    //Iniciar execucao do codigo button compilar
+    document.getElementById('compilar').addEventListener('click', function () {
+        main();
+    });
 }
 
 //Função para printar na tabela o conteudo do arquivo .obj
 function tabelaInstrucoes(texto) {
     let comentario = "";
     let linha = "";
-    let auxNum = 0;     //auxiliar para pegar primeiro/unico numero na linha
-    let auxNum2 = 0;    //auxiliar para pegar o segundo numero na linha 
-    let auxStr = "";    //auxiliar para desvios na linha
-    let palavra = "";
 
     var quantidade = document.getElementById("tabelaInstrucoes").rows.length;// está pré definido que será usado o tamanho total do arquivo
     if (quantidade > 1) { // quantidade representa o número indefinido de linhas que pode haver
@@ -168,103 +168,97 @@ function tabelaInstrucoes(texto) {
         '<td style="border: 2px solid; font-weight: bold">' + "Instruçao" + '</td>' +
         '<td style="border: 2px solid; font-weight: bold">' + "Comentario" + '</td></tr>';
 
-    for (i; i < itens.length; i++) {
+    for (i; i<itens.length; i++) {
         linha = itens[i];// espaços TAB definem colunas que serão consultadas
-        console.log(linha);
+        console.log("Linha: "+linha);
         if (linha.includes("START")) {
-            comentario = "(Iniciar programa principal): S:=-1 ";
+            comentario = "(Iniciar programa principal): S:= -1";
         } else if (linha.includes("NULL")) {
             comentario = "Nada";
         } else if (linha.includes("LDC")) {
-            comentario = "(Carregar constante): S:= s + 1 ; M [s]: = " + auxNum;
-            document.getElementById("formDados").innerHTML += "testeCarregarConstante\n";
+            comentario = "(Carregar constante): S:= s+1 ; M[s]:= k";
         } else if (linha.includes("LDV")) {
-            comentario = "(Carregar valor): S:=s + 1 ; M[s]:=M[" + auxNum + "]";
-            document.getElementById("formDados").innerHTML += "CarregarValor\n";
+            comentario = "(Carregar valor): S:= s+1 ; M[s]:= M[n]";
         } else if (linha.includes("ADD")) {
-            comentario = "(Somar): M[s-1]:=M[s-1] + M[s]; s:=s - 1";
+            comentario = "(Somar): M[s-1]:= M[s-1] + M[s]; s:= s-1";
         } else if (linha.includes("SUB")) {
-            comentario = "(Subtrair): M[s-1]:=M[s-1] - M[s]; s:=s - 1";
+            comentario = "(Subtrair): M[s-1]:= M[s-1] - M[s]; s:= s-1";
         } else if (linha.includes("MULT")) {
-            comentario = "(Multiplicar): M[s-1]:=M[s-1] * M[s]; s:=s - 1";
+            comentario = "(Multiplicar): M[s-1]:= M[s-1] * M[s]; s:= s-1";
         } else if (linha.includes("DIVI")) {
-            comentario = "(Dividir): M[s-1]:=M[s-1] div M[s]; s:=s - 1";
+            comentario = "(Dividir): M[s-1]:= M[s-1] div M[s]; s:= s-1";
         } else if (linha.includes("INV")) {
             comentario = "(Inverter sinal): M[s]:= -M[s]";
         } else if (linha.includes("AND")) {
-            comentario = "(Conjunção): se M [s-1] = 1 e M[s] = 1\n então M[s-1]:=1\n senão M[s-1]:=0; s:=s - 1";
+            comentario = "(Conjunção): se M[s-1]:= 1 e M[s] = 1 \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("OR")) {
-            comentario = "(Disjunção): se M[s-1] = 1 ou M[s] = 1\n então M[s-1]:=1\n senão M[s-1]:=0; s:=s - 1";
+            comentario = "(Disjunção): se M[s-1]:= 1 ou M[s] = 1 \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("NEG")) {
-            comentario = "(Negação): M[s]:=1 - M[s]";
+            comentario = "(Negação): M[s]:= 1 - M[s]";
         } else if (linha.includes("CME")) {
-            comentario = "(Comparar menor): se M[s-1] < M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar menor): se M[s-1] < M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("CMA")) {
-            comentario = "(Comparar maior): se M[s-1] > M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar maior): se M[s-1] > M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("CEQ")) {
-            comentario = "(Comparar igual): se M[s-1] = M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar igual): se M[s-1] = M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("CDIF")) {
-            comentario = "(Comparar desigual): se M[s-1] ≠ M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar desigual): se M[s-1] ≠ M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("CMEQ")) {
-            comentario = "(Comparar menor ou igual) se M[s-1] ≤ M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar menor ou igual) se M[s-1] ≤ M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("CMAQ")) {
-            comentario = "(Comparar maior ou igual): se M[s-1] ≥ M[s]\n então M[s-1]:=1\nsenão M[s-1]:=0; s:=s - 1";
+            comentario = "(Comparar maior ou igual): se M[s-1] ≥ M[s] \n então M[s-1]:= 1 \n senão M[s-1]:= 0; s:= s-1";
         } else if (linha.includes("HLT")) {
             comentario = "(Parar): “Para a execução da MVD”";
         } else if (linha.includes("STR")) {
-            comentario = "(Armazenar valor): M[" + auxNum + "]:=M[s]; s:=s-1";
-            document.getElementById("formDados").innerHTML += "teste\n";
+            comentario = "(Armazenar valor): M[n]:= M[s]; s:= s-1";
         } else if (linha.includes("JMP")) {
-            comentario = "(Desviar sempre): i:= " + auxStr;
+            comentario = "(Desviar sempre): i:= t";
         } else if (linha.includes("JMPF")) {
-            comentario = "(Desviar se falso): se M[s] = 0\nentão i:=" + auxStr + "\n senão i:=i + 1;\ns:=s-1";
+            comentario = "(Desviar se falso): se M[s]:= 0 \n então i:= t \n senão i:= i+1; \n s:= s-1";
         } else if (linha.includes("RD")) {
-            comentario = "(Leitura): S:=s + 1; M[s]:= “próximo valor de entrada”.";
-            document.getElementById("formEntrada").innerHTML += "testeEntrada\n";
+            comentario = "(Leitura): S:=s+1; M[s]:= “próximo valor de entrada”";
         } else if (linha.includes("PRN")) {
-            comentario = "(Impressão): “Imprimir M[s]”; s:=s-1 ";
-            document.getElementById("formSaida").innerHTML += "testeSaida\n";
+            comentario = "(Impressão): “Imprimir M[s]”; s:=s-1";
         } else if (linha.includes("ALLOC")) {
-            comentario = "(Alocar memória): Para k:=0 até " + auxNum2 + "-1\n faça {s:=s + 1; M[s]:=M[" + auxNum + "+k]}";
-            document.getElementById("formDados").innerHTML += "teste\n";
+            comentario = "(Alocar memória): Para k:= 0 até n-1\n faça {s:= s+1; M[s]:= M[m+k]}";
         } else if (linha.includes("DALLOC")) {
-            comentario = "(Desalocar memória): Para k:=" + auxNum2 + "-1 até 0\n faça {M[" + auxNum + "+k]:=M[s]; s:=s - 1}";
-            document.getElementById("formDados").innerHTML += "teste\n";
+            comentario = "(Desalocar memória): Para k:= n-1 até 0\n faça {M[m+k]:= M[s]; s:= s-1}";
         } else if (linha.includes("CALL")) {
-            comentario = "(Chamar procedimento ou função): S:=s + 1; M[s]:=i + 1; i:= " + auxStr;
+            comentario = "(Chamar procedimento ou função): S:= s+1; M[s]:= i+1; i:= t";
         } else if (linha.includes("RETURN")) {
-            comentario = "(Retornar de procedimento): i:=M[s]; s:=s - 1";
+            comentario = "(Retornar de procedimento): i:= M[s]; s:= s-1";
         }
 
-        document.getElementById("tabelaInstrucoes").innerHTML += '<tr><td style="text-align: center; vertical-align: middle;"><input type="checkbox" id="breakpoint" name="line" value="breakpoint"></td>' +
-            '<td>' + (i + 1) + '</td><td>' + linha + '</td><td>' + comentario + '</td></tr>';
-
+        document.getElementById("tabelaInstrucoes").innerHTML += '<tr><td style="text-align: center; vertical-align: middle;"><input type="checkbox" id="breakpoint" name="line" value="breakpoint"></td>'+
+                                                                '<td>' + (i + 1) + '</td><td>' + linha + '</td><td>' + comentario + '</td></tr>';
         comentario = "";
-        palavra = "";
     }
 }
 
 //=============================
 //===== Função basicas VM =====
 //=============================
-
+function start() {
+    s = -1;
+    document.getElementById("formDados").innerHTML = "Iniciando...";    
+}
 //Parar - “Para a execução da MVD”
-function hlt(params) {
+function hlt() {
     exec = false;
 }
 //Operação RD - Ultimo valor entrado pelo usuario.
 function rd() {
-    entryValue = readEntry();
+    let valor = document.getElementById("formEntrada").innerHTML += "Digite um valor: ";
     s = (s + 1);
-    memory[s] = entryValue;
+    memory[s] = valor;
 }
 //Saida - Impressão
 function prn() {
     // printValue(memory[s]);
     if (memory[s] == undefined) {
-        document.getElementById("formSaida").innerHTML += templateTerminal("");
+        document.getElementById("formSaida").innerHTML += "memoria undefined";
     } else {
-        document.getElementById("formSaida").innerHTML += templateTerminal(memory[s]);
+        document.getElementById("formSaida").innerHTML += memory[s];
     }
     s = (s - 1);
 }
@@ -278,16 +272,22 @@ function prn() {
 function ldc() {
     s = (s + 1);
     memory[s] = k;
+
+    document.getElementById("formDados").innerHTML += k+"\n";
 }
 //Carregar valor
 function ldv() {
     s = (s + 1);
     memory[s] = memory[n];
+
+    document.getElementById("formDados").innerHTML += memory[n]+"\n";
 }
 //Atribuição - Armazenar valor
 function str() {
     memory[n] = memory[s];
     s = (s - 1);
+
+    document.getElementById("formDados").innerHTML += memory[s]+"\n";
 }
 //Alocação de Variáveis
 function alloc() {
@@ -295,6 +295,7 @@ function alloc() {
         s = (s + 1);
         memory[s] = memory[instructions[i].parameters.p1 + j];
     }
+    document.getElementById("formDados").innerHTML += "teste\n";
 }
 //Desalocação de Variáveis
 function dalloc() {
@@ -303,6 +304,7 @@ function dalloc() {
         memory[instructions[i].parameters.p1 + j] = memory[s];
         s = (s - 1);
     }
+    document.getElementById("formDados").innerHTML += "teste\n";
 }
 
 //==========================
@@ -357,7 +359,7 @@ function or() {
     s = (s - 1);
 }
 //Operacao NEG
-function neg(params) {
+function neg() {
     memory[s] = 1 - memory[s];
 }
 
@@ -448,6 +450,8 @@ function jmpf() {
         // i = i - 1;
     }
     s = (s - 1);
+
+    document.getElementById("formDados").innerHTML += "\n";
 }
 
 //===============================
@@ -459,6 +463,8 @@ function call() {
     s = (s + 1);
     memory[s] = (i + 1);
     i = findLabel(instructions[i].parameters.p1).index;
+
+    document.getElementById("formDados").innerHTML += "teste\n";
 }
 
 //Retornar de procedimento
