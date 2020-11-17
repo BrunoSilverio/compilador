290355edf3f4612d5a0ac2a5b4f89caefcf350ae
Bruno C. Silvério <brunocsilverio@hotmail.com>
2020-11-17 17:52:08 -0300

    iniciando posFixo

--- Compilador/js/semantico/posFixo.js
+++ Compilador/js/semantico/posFixo.js
@@ -1,23 +1,67 @@
 // Compilador, copyright (c) by Bruno Camilo Silverio & Daniel de Arruda Fraga
 
-
-//Pos fixo final
+//lista para guardar a expressao infixa na forma pos fixa
 let posFixo = [];
 
-//Pos fixo auxiliar
-let posFixoTemp = [];
+//pilha para gerenciar os operadores
+let posFixoOperadores = [];
+
+//Precedencia do Operadores (7,6,5,...,1)
+let precOperadores = [
+    {
+        operacao: ["-$","+$","not"],
+        camada: 7
+    },
+    {
+        operacao: ["*","div"],
+        camada: 6
+    },
+    {
+        operacao: ["-","+"],
+        camada: 5
+    },
+    {
+        operacao: ["<","<=",">",">="],
+        camada: 4
+    },
+    {
+        operacao: ["=","!="],
+        camada: 3
+    },
+    {
+        operacao: ["and"],
+        camada: 2
+    },
+    {
+        operacao: ["or"],
+        camada: 1
+    },
+];
+
+function posFixoGerador(expressao) {
+    
+    //recebe a expressao infixa por parametro
+    //separa a expressao nos espaços (split(" "))
+    //valida termo por termo, utlizando 'for'
+
+ 
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        //identifica operando(var) ou operador(+,-,*,div...)
+
+        //caso seja operando (variavel)
+
+
+        //caso seja um operador
+        
+    }
+}
 
 function limpaPosFixo() {
     posFixo = [];
 }
 
 function limpaPosFixoTemp() {
-    posFixoTemp = [];
-}
-
-
-function posFixo(expressao) {
-    //recebe expressao infixa
-
-
+    posFixoOperadores = [];
 }
